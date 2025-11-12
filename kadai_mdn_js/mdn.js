13feef84714970c45b35e14285fc6e05b580d2a1
Hachii-0321 <hattashota1321@outlook.com>
2025-11-12 10:44:15 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -2,6 +2,6 @@ const now = new Date();
 const year = now.getFullYear();
 const month = now.getMonth() + 1;
 const date = now.getDate();
-const nowDisplay = year.toString() + '年' + month.toString() + '月' + date.toString() + '日'
+const nowDisplay = year.toString() + '年' + month.toString() + '月' + date.toString() + '日';
 
-console.log(nowDisplay)
+console.log(nowDisplay);
