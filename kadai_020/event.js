bd35474bff28e308c97b82c655a195cb96bbc8a9
Hachii-0321 <hattashota1321@outlook.com>
2025-11-13 13:59:57 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('btn');
+const h2 = document.getElementById('text');
 
 btn.addEventListener('click', () => {
-    const h2 = document.getElementById('text');
     h2.textContent = 'ボタンをクリックしました';
 });
